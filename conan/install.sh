ac9e2b905882fccd2c3ff0c7ee77f9fc99eb55f4
Spide85 <spide@mailbox.org>
2019-07-26 21:50:49 +0200

    CI

--- conan/install.sh
+++ conan/install.sh
@@ -5,6 +5,6 @@ export CXX=clang++
 export CC=clang
 conan install ../conan --build missing
 cd ../src
-export PKG_CONFIG_PATH=/home/andy/Projects/openFactory/build/
+export PKG_CONFIG_PATH==$(realpath ../build/)
 meson ../build
 ln -s build/compile_commands.json compile_commands.json
