f67ed34012005645d63246d71354c6c8bac7265d
Spide85 <spide@mailbox.org>
2019-07-26 23:06:21 +0200

    Fixed meson conan boost bug

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -8,8 +8,8 @@ class OpenFactoryConan(ConanFile):
         "eigen/3.3.7@conan/stable",
         "fmt/5.3.0@bincrafters/stable",
         "spdlog/1.3.1@bincrafters/stable",
-        # "sdl2/2.0.9@bincrafters/stable",
-        # "boost/1.70.0@conan/stable", meson currently does not support conan + boost. see https://github.com/mesonbuild/meson/issues/5438
+        "sdl2/2.0.9@bincrafters/stable",
+        "boost/1.70.0@conan/stable",
     ]
 
     def build(self):
--- conan/install.sh
+++ conan/install.sh
@@ -6,5 +6,6 @@ export CC=clang
 conan install ../conan --build missing
 cd ../src
 export PKG_CONFIG_PATH=$(realpath ../build/)
+export BOOST_ROOT=$(pkg-config --variable=prefix boost)
 meson ../build
 ln -s build/compile_commands.json compile_commands.json
