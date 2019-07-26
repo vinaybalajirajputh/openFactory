9ca6cfdc08d4ec146c6b7acd50b19e4e0d9a425e
Spide85 <spide@mailbox.org>
2019-07-26 22:08:09 +0200

    CI

--- conan/install.sh
+++ conan/install.sh
@@ -5,6 +5,6 @@ export CXX=clang++
 export CC=clang
 conan install ../conan --build missing
 cd ../src
-export PKG_CONFIG_PATH==$(realpath ../build/)
+export PKG_CONFIG_PATH=$(realpath ../build/)
 meson ../build
 ln -s build/compile_commands.json compile_commands.json
